6f66a0a2a59436e6d1d7f1144f917040bfd841a4
patflynn89 <xbl_bachhuber@hotmail.com>
2019-11-06 19:45:32 +0100

    jquery for showing and hiding menu-nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,10 +6,14 @@ $(document).ready(function () {
     $(".menu-btn").click(function() {
         if (!showMenue) {
             $(".menu-btn").addClass("close");
-            showMenue = true;         
+            showMenue = true;
+            //$(".menu-nav").show();         
+            $(".menu-nav").slideDown("slow");
         } else {
             $(".menu-btn").removeClass("close");
             showMenue = false;
+            //$(".menu-nav").hide();
+            $(".menu-nav").slideUp("slow");
         }
     });
 });
